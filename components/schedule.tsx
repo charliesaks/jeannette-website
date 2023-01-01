4d56ca562fd458d1f60a6f6720a83c83dc6db2c4
Charlie Sakamaki <csakamaki@hawaii.rr.com>
2022-12-31 15:56:39 -1000

    Update schedule Jan 2023

--- components/schedule.tsx
+++ components/schedule.tsx
@@ -9,8 +9,8 @@ function Schedule() {
         </h1>
         <div className="pt-5 pb-10">
           <Image
-            src="/images/site/Schedule-2022-12.jpg"
-            alt="Jeannette Trevias Schedule October 2022"
+            src="/images/site/Schedule-2023-01.jpg"
+            alt="Jeannette Trevias Schedule January 2023"
             width={600}
             height={400}
             layout="responsive"
