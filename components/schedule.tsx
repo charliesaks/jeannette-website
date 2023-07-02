d5c313ffc2c37680c9b323ad22f2d021f70275d3
Charlie Sakamaki <csakamaki@hawaii.rr.com>
2023-07-01 18:11:03 -1000

    Fix centering of schedule

--- components/schedule.tsx
+++ components/schedule.tsx
@@ -8,7 +8,7 @@ function Schedule() {
         <h1 className="pt-24 text-2xl md:text-5xl text-center text-gray-500 ">
           Schedule
         </h1>
-        <div className="pt-5 pb-10">
+        <div className="pt-5 pb-10 text-center">
           <Image
             src={schedulePic}
             alt="Jeannette Trevias Schedule March 2023"
